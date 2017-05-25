77e95a967fa1bc5985b7e6879e54f86a9a95a6c8
Seganok <msn3862@mail.ru>
2017-05-25 16:53:23 +0300

    потом с js разберусь

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href="/bootstrap-3.3.7-dist/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+    <link href="/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css" type="text/css" rel="stylesheet">
+    <link href="/css/admin.min.css" type="text/css" rel="stylesheet">
+
+	<title>Жиротопка</title>
+	
+</head>
+<body class="nav-md">
+
+	@section("content")
+
+    @show
+
+    <script type="text/javascript" src="/js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/js/admin.min.js"></script>
+    <script type="text/javascript" src="/js/rocket.min.js"></script>
+
+</body>
+</html>
