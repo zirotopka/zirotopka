dee92cd7de86934328d6a691dc624e14cee7f5fd
Seganok <msn3862@mail.ru>
2017-05-25 23:05:49 +0300

    dash fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,9 @@
 
 	<link href="/bootstrap-3.3.7-dist/css/bootstrap.min.css" type="text/css" rel="stylesheet">
     <link href="/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css" type="text/css" rel="stylesheet">
+    <link href="/font-awesome-4.7.0/css/font-awesome.min.css" type="text/css" rel="stylesheet">
     <link href="/css/admin.min.css" type="text/css" rel="stylesheet">
+
 
 	<title>Жиротопка</title>
 	
@@ -18,9 +20,10 @@
     @show
 
     <script type="text/javascript" src="/js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="/js/rocket.min.js"></script>
+
     <script type="text/javascript" src="/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="/js/admin.min.js"></script>
-    <script type="text/javascript" src="/js/rocket.min.js"></script>
 
 </body>
 </html>
