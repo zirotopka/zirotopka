7870dda3729839cd0925993765b04525ae7dbad8
Seganok <msn3862@mail.ru>
2017-05-31 17:46:45 +0300

    New navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,12 +2,15 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link href="/bootstrap-3.3.7-dist/css/bootstrap.min.css" type="text/css" rel="stylesheet">
     <link href="/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css" type="text/css" rel="stylesheet">
     <link href="/font-awesome-4.7.0/css/font-awesome.min.css" type="text/css" rel="stylesheet">
-    <link href="/css/admin.min.css" type="text/css" rel="stylesheet">
+    <link href="/navigation/css/component.css" type="text/css" rel="stylesheet">
+    <link href="/navigation/css/user.css" type="text/css" rel="stylesheet">
 
 
 	<title>Жиротопка</title>
@@ -17,154 +20,49 @@
     @show
 	
 </head>
-<body class="nav-md">
-    <div class="container body">
-        <div class="main_container">
-                <!--Left Menu-->
-            <div class="col-md-2 left_col">
-                <div class="left_col scroll-view">
-                    <div class="navbar nav_title" style="border: 0;">
-                        <a href="/" class="site_title"><i class="fa fa-paw"></i> <span>Жиротопка</span></a>
-                    </div>
-                        <!-- /menu profile quick info -->
-                        <br>
-                    <!-- sidebar menu -->
-                    <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-                       <div class="menu_section active">
-                            <h3>General</h3>
-                            <ul class="nav side-menu" style="">
-                                <li class="active"><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu" style="display: block;">
-                                        <li class="current-page"><a href="index.html">Dashboard</a></li>
-                                        <li><a href="index2.html">Dashboard2</a></li>
-                                        <li><a href="index3.html">Dashboard3</a></li>
-                                    </ul>
-                                </li>
-                                <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li><a href="form.html">General Form</a></li>
-                                        <li><a href="form_advanced.html">Advanced Components</a></li>
-                                        <li><a href="form_validation.html">Form Validation</a></li>
-                                        <li><a href="form_wizards.html">Form Wizard</a></li>
-                                        <li><a href="form_upload.html">Form Upload</a></li>
-                                        <li><a href="form_buttons.html">Form Buttons</a></li>
-                                    </ul>
-                                </li>
-                                <li><a><i class="fa fa-desktop"></i> UI Elements <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li><a href="general_elements.html">General Elements</a></li>
-                                        <li><a href="media_gallery.html">Media Gallery</a></li>
-                                        <li><a href="typography.html">Typography</a></li>
-                                        <li><a href="icons.html">Icons</a></li>
-                                        <li><a href="glyphicons.html">Glyphicons</a></li>
-                                        <li><a href="widgets.html">Widgets</a></li>
-                                        <li><a href="invoice.html">Invoice</a></li>
-                                        <li><a href="inbox.html">Inbox</a></li>
-                                        <li><a href="calendar.html">Calendar</a></li>
-                                    </ul>
-                                </li>
-                                <li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li><a href="tables.html">Tables</a></li>
-                                        <li><a href="tables_dynamic.html">Table Dynamic</a></li>
-                                    </ul>
-                                </li>
-                                <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li><a href="chartjs.html">Chart JS</a></li>
-                                        <li><a href="chartjs2.html">Chart JS2</a></li>
-                                        <li><a href="morisjs.html">Moris JS</a></li>
-                                        <li><a href="echarts.html">ECharts</a></li>
-                                        <li><a href="other_charts.html">Other Charts</a></li>
-                                    </ul>
-                                </li>
-                                <li><a><i class="fa fa-clone"></i>Layouts <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li><a href="fixed_sidebar.html">Fixed Sidebar</a></li>
-                                        <li><a href="fixed_footer.html">Fixed Footer</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
+<body>
+        <div id="st-container" class="st-container">
+            <!--    
+                example menus 
+                these menus will be on top of the push wrapper
+            -->
+
+            
+            <nav class="st-menu st-effect-2 col-xs-5 col-sm-5 col-lg-2 col-md-2" id="menu-2">
+                <h2 class="icon icon-stack">Sidebar</h2>
+                <ul>
+                    <li><a class="icon icon-data" href="#">Data Management</a></li>
+                </ul>
+            </nav>
+           <!-- content push wrapper -->
+            <div class="st-pusher">
+                <div class="st-content"><!-- this is the wrapper for the content -->
+                    <div class="st-content-inner"><!-- extra div for emulating position:fixed of the menu -->
+                        <!-- Top Navigation -->
+                        <div class="codrops-top clearfix ">
+                            <div id="st-trigger-effects" class="column">
+                                <button data-effect="st-effect-2">
+                                    <i class="fa fa-bars fa-2x" aria-hidden="true"></i>
+                                </button>
+                            </div>
                         </div>
-                        <div class="menu_section">
-                            <h3>Live On</h3>
-                            <ul class="nav side-menu">
-                                <li><a><i class="fa fa-bug"></i> Additional Pages <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li><a href="e_commerce.html">E-commerce</a></li>
-                                        <li><a href="projects.html">Projects</a></li>
-                                        <li><a href="project_detail.html">Project Detail</a></li>
-                                        <li><a href="contacts.html">Contacts</a></li>
-                                        <li><a href="profile.html">Profile</a></li>
-                                    </ul>
-                                </li>
-                                <li><a><i class="fa fa-windows"></i> Extras <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li><a href="page_403.html">403 Error</a></li>
-                                        <li><a href="page_404.html">404 Error</a></li>
-                                        <li><a href="page_500.html">500 Error</a></li>
-                                        <li><a href="plain_page.html">Plain Page</a></li>
-                                        <li><a href="login.html">Login Page</a></li>
-                                        <li><a href="pricing_tables.html">Pricing Tables</a></li>
-                                    </ul>
-                                </li>
-                                <li><a><i class="fa fa-sitemap"></i> Multilevel Menu <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li><a href="#level1_1">Level One</a></li>
-                                        <li><a>Level One<span class="fa fa-chevron-down"></span></a>
-                                            <ul class="nav child_menu">
-                                                <li class="sub_menu"><a href="level2.html">Level Two</a></li>
-                                                <li><a href="#level2_1">Level Two</a></li>
-                                                <li><a href="#level2_2">Level Two</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="#level1_2">Level One</a></li>
-                                    </ul>
-                                </li>                  
-                                <li><a href="javascript:void(0)"><i class="fa fa-laptop"></i> Landing Page <span class="label label-success pull-right">Coming Soon</span></a></li>
-                            </ul>
-                        </div>
-
-                    </div>
-                        <!-- /sidebar menu -->
-
-                        <!-- /menu footer buttons -->
-                    <div class="sidebar-footer hidden-small">
-                        <a data-toggle="tooltip" data-placement="top" title="" data-original-title="Settings">
-                            <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
-                        </a>
-                        <a data-toggle="tooltip" data-placement="top" title="" data-original-title="FullScreen">
-                            <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
-                        </a>
-                        <a data-toggle="tooltip" data-placement="top" title="" data-original-title="Lock">
-                            <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
-                        </a>
-                        <a data-toggle="tooltip" data-placement="top" title="" href="login.html" data-original-title="Logout">
-                            <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-                        </a>
-                    </div>
-                        <!-- /menu footer buttons -->
-                </div>
-            </div>
-            @section("content")
-
-            @show    
-        </div>
-    </div>
-
-
-
-
-
-
+                        <div class="main clearfix">
+                            @section("content")
+                            @show    
+                        </div> 
+                    </div><!-- /st-content-inner -->
+                </div><!-- /st-content -->
+            </div><!-- /st-pusher -->
+        </div><!-- /st-container -->
+            @include('layouts.topbar')
     @section('js')
         @yield('js')
     @show
     <script type="text/javascript" src="/js/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="/js/rocket.min.js"></script>
-
     <script type="text/javascript" src="/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="/js/admin.min.js"></script>
+    <script type="text/javascript" src="/navigation/js/classie.js"></script>
+    <script type="text/javascript" src="/navigation/js/sidebarEffects.js"></script>
+
 
 </body>
 </html>
---
+++ resources/views/layouts/reg_modal.blade.php
@@ -0,0 +1,14 @@
+<div class="modal fade" tabindex="-1" role="dialog">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-body">		
+				<input type="checkout" id="oferta">
+				<h4>Яознакомился с офертой</h4>
+				<input type="checkout">
+				<h4>Мне больше 14 лет</h4>
+				<br>
+				<h3>Когда хотите начать</h3>
+			</div>
+		</div>
+	</div>
+</div>
