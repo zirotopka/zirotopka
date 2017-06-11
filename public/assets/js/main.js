59e65e5055b4e6061b6b16e59bc1d5167c2a76f7
Олег Штолин <kasatka567@gmail.com>
2017-06-11 16:37:56 +0300

    merge

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 	$('.tooltipstered').tooltipster({
-   theme: 'tooltipster-borderless',
+   theme: ['tooltipster-borderless', 'tooltipster-borderless-customized'],
    delay: 200,
    maxWidth: 400,
    side: 'bottom',
 	});
 
+	$('#choose_programe_form').modal('show')
 	$( "#program_date_input" ).datepicker();
 })
