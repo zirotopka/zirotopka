4f74f2190be3d031b5c59d5a3593ce49620c96e4
Олег Штолин <kasatka567@gmail.com>
2017-06-11 14:13:01 +0300

    me=rge

--- resources/views/layouts/choose_program_form.blade.php
+++ resources/views/layouts/choose_program_form.blade.php
@@ -1,7 +1,7 @@
 <div class="modal fade" id="choose_programe_form" tabindex="-1" role="dialog" aria-labelledby="ProgramModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
-      <div class="modal-body">
+      <div class="modal-body choose-program-form">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
