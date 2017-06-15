7b135b54a481bf1293c5a6ff960d617eb45faef6
Seganok <msn3862@mail.ru>
2017-06-15 18:50:20 +0300

    Изменил положение плей

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -60,7 +60,7 @@
                             </div>
                        @if ($user = Sentinel::check())
                             <!-- <div class="immunitet col-lg-3 col-md-3 hidden-sm hidden-xs"> -->
-                            <div class="tooltipstered immunitet col-lg-3 col-md-3 hidden-sm hidden-xs" data-tooltip-content="#immun_tooltip_content">
+                            <div class="immunitet col-lg-3 col-md-3 hidden-sm hidden-xs">
                                 <span class="nav-text" style="position: absolute; margin: 0 0 0 -11em;">Ваши иммунитеты:</span>
                                <!--  <div class="hearts col-lg-6 col-md-6">  -->
                                     <?php 
