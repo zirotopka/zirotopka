6d4372096d6f1fe07da9788a35e752bc0caa3689
Seganok <msn3862@mail.ru>
2017-06-21 15:35:40 +0300

    merge

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -10,34 +10,32 @@ $(document).ready(function(){
 	$('#choose_programe_form').modal('show');
 	$("#program_bnr").attr('src',"/image/test/r.one_start.png");
 	$('.selectpicker').on('hidden.bs.select', function (e) {
-  	var	program_id = $('.selectpicker').val();
-	$("#program_id").change(function () {
-        console.log();
-		jQuery.ajax({
-		            type: "post",
-		            url: '/program/get_program',
-		            data: {id: program_id},
-		            success: function (result) {
-		           		
-		            }
-		        });   
-	});
-  		if (program_id == 1) {
-  			$("#program_bnr").attr('src',"/image/test/r.one_start.png");
-  				
-  		}
-  		else if (program_id == 2){
-  			$("#program_bnr").attr('src',"/image/test/r.one_pro.png");
-  		}
-  		else if (program_id == 3){
-  			$("#program_bnr").attr('src',"/image/test/r.one_run.png");
-  		}
-  		else if (program_id == 4){
-  			$("#program_bnr").attr('src',"/image/test/r.one_run+.png");
-  		}
-  		else if (program_id == 5){
-  			$("#program_bnr").attr('src',"/image/test/r.one_power.png");
-  		}
+	  	var	program_id = $('.selectpicker').val();
+
+	        console.log();
+			jQuery.ajax({
+			            type: "post",
+			            url: '/program/get_program',
+			            data: {id: program_id},
+			            success: function (result) {
+			           		
+			            }
+			        });   
+		if (program_id == 1) {
+	  		$("#program_bnr").attr('src',"/image/test/r.one_start.png");
+	  	}
+	  	else if (program_id == 2){
+	  		$("#program_bnr").attr('src',"/image/test/r.one_pro.png");
+	  	}
+	  	else if (program_id == 3){
+	  		$("#program_bnr").attr('src',"/image/test/r.one_run.png");
+	  	}
+	  	else if (program_id == 4){
+	  		$("#program_bnr").attr('src',"/image/test/r.one_run+.png");
+	  	}
+	  	else if (program_id == 5){
+	  		$("#program_bnr").attr('src',"/image/test/r.one_power.png");
+	  	}
 	});
 
 });
