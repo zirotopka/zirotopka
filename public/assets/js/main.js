449b11b70ed635fd7a36e515176319ccbc43ba58
Олег Штолин <kasatka567@gmail.com>
2017-06-21 15:33:14 +0300

    аякс

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -10,36 +10,36 @@ $(document).ready(function(){
 	$('#choose_programe_form').modal('show');
 	$("#program_bnr").attr('src',"/image/test/r.one_start.png");
 	$('.selectpicker').on('hidden.bs.select', function (e) {
-  	var	program_id = $('.selectpicker').val();
-	$("#program_id").change(function () {
-        console.log();
-		jQuery.ajax({
-		            type: "post",
-		            url: '/program/get_program',
-		            data: {id: program_id},
-		            success: function (result) {
-		           		
-		            }
-		        });   
-	});
-  		if (program_id == 1) {
-  			$("#program_bnr").attr('src',"/image/test/r.one_start.png");
-  				
-  		}
-  		else if (program_id == 2){
-  			$("#program_bnr").attr('src',"/image/test/r.one_pro.png");
-  		}
-  		else if (program_id == 3){
-  			$("#program_bnr").attr('src',"/image/test/r.one_run.png");
-  		}
-  		else if (program_id == 4){
-  			$("#program_bnr").attr('src',"/image/test/r.one_run+.png");
-  		}
-  		else if (program_id == 5){
-  			$("#program_bnr").attr('src',"/image/test/r.one_power.png");
-  		}
+		if (program_id == 1) {
+			$("#program_bnr").attr('src',"/image/test/r.one_start.png");
+				
+		}
+		else if (program_id == 2){
+			$("#program_bnr").attr('src',"/image/test/r.one_pro.png");
+		}
+		else if (program_id == 3){
+			$("#program_bnr").attr('src',"/image/test/r.one_run.png");
+		}
+		else if (program_id == 4){
+			$("#program_bnr").attr('src',"/image/test/r.one_run+.png");
+		}
+		else if (program_id == 5){
+			$("#program_bnr").attr('src',"/image/test/r.one_power.png");
+		}
 	});
 
+  $("#program_id").change(function () {
+    var program_id = $('.selectpicker').val();
+    console.log(program_id);
+    jQuery.ajax({
+      type: "post",
+      url: '/program/get_program',
+      data: {id: program_id},
+      success: function (result) {
+        console.log(result);
+      }
+    });   
+  });
 });
 
 
