5409047352dcc2dd6e0d6742a4ad0cc0a0a70e4e
Олег Штолин <kasatka567@gmail.com>
2017-07-02 13:09:11 +0300

    Api сообщений

--- app/Message.php
+++ app/Message.php
@@ -8,4 +8,14 @@ class Message extends Model
 {
     protected $table = 'messages';
     protected $primaryKey = 'id';
+
+    public function incomes()
+    {
+        return $this->belongsTo('App\User', 'id', 'recipient_id');
+    }
+
+    public function outputs()
+    {
+        return $this->belongsTo('App\User', 'id', 'sender_id');
+    }
 }
