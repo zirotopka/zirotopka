12498e4363ff3c3f38fce3e5895b2ae8c04b09d8
Seganok <msn3862@mail.ru>
2017-07-05 20:34:42 +0300

    qwe

--- resources/views/privat_office/_partials/output_message.blade.php
+++ resources/views/privat_office/_partials/output_message.blade.php
@@ -24,7 +24,7 @@
 						<p class="sended">ОТПРАВЛЕННЫЕ</p>
 					</li>
 					<li class="min_msg_cont">
-						<a href="#" class="min_msg_cont">
+						<a href="#">
 						<div class="min_left_col col-lg-6 col-md-6 col-sm-6 col-xs-6">
 							<p class="who_send">Тренер</p>
 							<a class="answer_msg">Ответить на вопрос</a>
