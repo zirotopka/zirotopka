940a156a001c6a1ab35126f21031063d4ecd627c
Олег Штолин <kasatka567@gmail.com>
2017-08-04 15:28:55 +0300

    добавил ключи

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -42,7 +42,11 @@ class Test extends Command
         $answ = Yandex::send_payments(123, 1000, '', true);
 
         //$cert = openssl smime -sign -signer вашклиентскийсерт.cer -inkey private.key -nochain -nocerts -outform PEM -nodetach -passin pass:password_for_private_key
-        $cert = openssl_pkcs7_sign($answ['text'],public_path().'/signed.txt','/Users/olegstolin/Sites/zirotopka/SSL/certnew.cer','/Users/olegstolin/Sites/zirotopka/SSL/private.key',[]);
+        $cert = openssl_pkcs7_sign($answ['text'],
+                                   public_path().'/signed.txt',
+                                   public_path().'/SSL/certnew.cer',
+                                   public_path().'/SSL/private.key',
+                                   [], PKCS7_NOSIGS);
         dd($cert);
         // $geo = new GeoIp;
         // $geo->setIp('178.219.244.202');
