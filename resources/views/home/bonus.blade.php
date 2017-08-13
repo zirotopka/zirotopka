29bfe4e102a525de50f3d3e1c7b6f52e7836a9aa
Seganok <msn3862@mail.ru>
2017-08-13 09:55:40 +0300

    qweewq

--- resources/views/home/bonus.blade.php
+++ resources/views/home/bonus.blade.php
@@ -53,8 +53,6 @@
                         </p>
                     </div>
                     <div class="mobimg visible-xs">
-<?xml version="1.0" encoding="utf-8"?>
-<!-- Generator: Adobe Illustrator 20.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
 <svg version="1.1" id="Слой_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
      viewBox="0 0 2000 2000" style="enable-background:new 0 0 2000 2000;" xml:space="preserve">
 <style type="text/css">
@@ -366,8 +364,7 @@
                         </ul>
                     </div>
                     <div class="carrows hidden-xs">
-<?xml version="1.0" encoding="utf-8"?>
-<!-- Generator: Adobe Illustrator 20.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+
 <svg version="1.1" id="crws" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
      viewBox="0 0 2000 2000" style="enable-background:new 0 0 2000 2000;" xml:space="preserve">
 <style type="text/css">
