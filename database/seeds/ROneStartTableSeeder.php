204115f0cd6b44044201879b697253385f7f70ec
Seganok <msn3862@mail.ru>
2017-09-11 10:15:01 +0300

    qweqwe

--- database/seeds/ROneStartTableSeeder.php
+++ database/seeds/ROneStartTableSeeder.php
@@ -47,8 +47,12 @@ class ROneStartTableSeeder extends Seeder
     	$programm->id = 1;
     	$programm->slug = 'ROneStart';
     	$programm->name = 'R.ONE START';
-    	$programm->description = 'Программа предназначенная для новичков';
+    	$programm->description = 'Программа тренировок подойдет для всех вне зависимости от уровня подготовки, в том числе для новичков или тех, кто очень давно не занимался спортом и только сейчас готов стать реформатором. Попробуйте бесплатно в течение тестового периода.';
         $programm->cost = 2500;
+        $programm->days =28;
+        $programm->trainings =9;
+        $programm->day_off =8;
+        $programm->tasks =9;
 
         if ( $programm->save() ) {
     		$programm_id = $programm->id;
