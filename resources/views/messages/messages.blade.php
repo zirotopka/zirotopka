9f9e1f56cfb42dbe3e4e3ec2a73ed69c456a57af
Seganok <msn3862@mail.ru>
2017-09-21 18:40:56 +0300

    Одновил сообщения, добавил окно рейтинга, изменил пользовательское соглашение.

--- resources/views/messages/messages.blade.php
+++ resources/views/messages/messages.blade.php
@@ -1,18 +1,32 @@
+<?php
+	$newor = '';
+	$inpor = '';
+	$outor = '';
+	if (Request::path() == 'messages/new'){
+		$newor = 'orange';
+	}
+	elseif(Request::path() == 'messages/1'){
+		$inpor = 'orange';
+	}
+	elseif(Request::path() == 'messages/2'){
+		$outor = 'orange';
+	}
+?>
 	<div class="little_menu">
 		<ul>
 			<li class="hidden-xs hidden-sm"></li>
 			<a href="/messages/new">	
-				<li class="send">
+				<li class="send {{$newor}}">
 					<img src="/ico/add_msg.png" alt="" class="add_img">
 				</li>
 			</a>
 			<a href="/messages/2">
-				<li class="output">
+				<li class="output {{$outor}}">
 					<img src="/ico/input_mess.png" alt="">
 				</li>
 			</a>
 			<a href="/messages/1">
-				<li class="input">
+				<li class="input {{$inpor}}">
 					<img src="/ico/mess.png" alt="">
 				</li>
 			</a>
