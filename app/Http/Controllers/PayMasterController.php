a4f46ff50478bdf0fa8d640fa04e293028bdc7bd
Олег Штолин <kasatka567@gmail.com>
2017-09-24 17:01:24 +0300

    проверка  подтверждения

--- app/Http/Controllers/PayMasterController.php
+++ app/Http/Controllers/PayMasterController.php
@@ -7,18 +7,21 @@ use Illuminate\Http\Request;
 class PayMasterController extends Controller
 {
     public function notification (Request $request) {
-    	\Log::info('notification:'.json_encode($request->all()));
+        \Log::info('notification:'.json_encode($request->all()));
     }
 
     public function success (Request $request) {
-    	\Log::info('success:'.json_encode($request->all()));
+        \Log::info('success:'.json_encode($request->all()));
+
+        return response('YES', 200)
+                  ->header('Content-Type', 'text/plain');
     }
 
     public function invoice (Request $request) {
-    	\Log::info('invoice:'.json_encode($request->all()));
+        \Log::info('invoice:'.json_encode($request->all()));
     }
 
     public function failure (Request $request) {
-    	\Log::info('failure:'.json_encode($request->all()));
+        \Log::info('failure:'.json_encode($request->all()));
     }
 }
