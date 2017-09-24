7376257bdf317d5cd22a9cf06cc684cb3847c556
Олег Штолин <kasatka567@gmail.com>
2017-09-24 18:06:28 +0300

    сделал модуль

--- app/Http/Controllers/PayMasterController.php
+++ app/Http/Controllers/PayMasterController.php
@@ -3,20 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Accrual;
 
 use Illuminate\Http\Request;
 
 class PayMasterController extends Controller
-{
+{	
+
+	// [2017-09-24 14:07:12] local.INFO: notification:{"LMI_MERCHANT_ID":"273ebbb9-8678-4e29-860f-91eb86630816","LMI_PAYMENT_SYSTEM":"503","LMI_CURRENCY":"RUB","LMI_PAYMENT_AMOUNT":"2500.00","LMI_PAYMENT_DESC":"ReferMoney","LMI_SYS_PAYMENT_DATE":"2017-09-24T14:07:11","LMI_SYS_PAYMENT_ID":"89667424","LMI_PAID_AMOUNT":"2500.00","LMI_PAID_CURRENCY":"RUB","LMI_SIM_MODE":"0","LMI_PAYER_IDENTIFIER":"4XXXXXXXXXXX0010","LMI_PAYMENT_METHOD":"Test","PAYER_ID":"39","LMI_HASH":"B33BsjMgXiGBo0NS8\/NF7w=="}  
     public function notification (Request $request) {
-        \Log::info('notification:'.json_encode($request->all()));
+    	$LMI_PAID_AMOUNT = (float) $request->get('LMI_PAYMENT_AMOUNT');
+        $userID = $request->get('PAYER_ID');
+
+        $accrual = new Accrual;
+        $accrual->sum = $LMI_PAID_AMOUNT;
+        $accrual->user_id = $userID;
+        $accrual->type_id = 1;
+        $accrual->comment = 'Успешная оплата программы';
+        $accrual->accruals_json = json_encode($request->all());
+        if (!$accrual->sum()) {
+        	\Log::warning('PayMasterController: Не сохранен платеж в системе. Json: '.json_encode($request->all()));
+        }
+
+        return true;
     }
 
+    // [2017-09-24 14:07:48] local.INFO: success:{"LMI_MERCHANT_ID":"273ebbb9-8678-4e29-860f-91eb86630816","LMI_CURRENCY":"RUB","LMI_PAYMENT_AMOUNT":"2500.00","LMI_SYS_PAYMENT_DATE":"2017-09-24T14:07:12","LMI_SYS_PAYMENT_ID":"89667424","PAYER_ID":"39"}  
     public function success (Request $request) {
-        \Log::info('success:'.json_encode($request->all()));
+        $userID = $request->get('PAYER_ID');
 
-        return response('YES', 200)
-                  ->header('Content-Type', 'text/plain');
+        $user = User::select('slug','id'->where('id','=',$userID))->first();
+
+        if (empty($user)) {
+        	return redirect('/');
+        }
+
+        return redirect('/'.$user->slug);
     }
 
     // [2017-09-24 14:07:11] local.INFO: invoice:{"LMI_MERCHANT_ID":"273ebbb9-8678-4e29-860f-91eb86630816","LMI_PAYMENT_SYSTEM":"503","LMI_CURRENCY":"RUB","LMI_PAYMENT_AMOUNT":"2500.00","LMI_PAYMENT_DESC":"ReferMoney","LMI_PAID_AMOUNT":"2500.00","LMI_PAID_CURRENCY":"RUB","LMI_PREREQUEST":"1","LMI_PAYMENT_METHOD":"Test","PAYER_ID":"39"} 
@@ -60,6 +82,6 @@ class PayMasterController extends Controller
     }
 
     public function failure (Request $request) {
-        \Log::info('failure:'.json_encode($request->all()));
+        return redirect('/');
     }
 }
