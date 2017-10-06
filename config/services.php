1ccb224b626b087711eb2af15487f6ce87fda38b
Олег Штолин <kasatka567@gmail.com>
2017-10-06 19:41:55 +0300

    поставил фесбук

--- config/services.php
+++ config/services.php
@@ -41,4 +41,10 @@ return [
         'redirect' => env('VKONTAKTE_REDIRECT_URI'),  
     ],
 
+    'facebook' => [
+        'client_id' => env('FACEBOOK_KEY'),
+        'client_secret' => env('FACEBOOK_SECRET'),
+        'redirect' => env('FACEBOOK_REDIRECT_URI'),  
+    ],
+
 ];
