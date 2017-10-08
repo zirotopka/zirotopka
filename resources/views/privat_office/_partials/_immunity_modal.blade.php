ca703915dfe68753a7f42f9916a8bfb0417f8ec1
Unknown <msn3862@mail.ru>
2017-10-08 16:49:52 +0300

    Обноил стили окон, исправил swiper

--- resources/views/privat_office/_partials/_immunity_modal.blade.php
+++ resources/views/privat_office/_partials/_immunity_modal.blade.php
@@ -1,6 +1,6 @@
 <div class="modal fade" id="immunity-modal" tabindex="-1" role="dialog" aria-labelledby="withdrawal_modal">
   <div class="modal-dialog bal_modal_width" role="document">
-    <div class="modal-content bal_modal_height">
+    <div class="modal-content balance_modal_height">
       <div class="modal-body">
         <button type="button" class="close cls_mod_btn" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 		<div class="input_money">
@@ -11,7 +11,7 @@
 				  {{ csrf_field() }}
 				  <input type="number" placeholder="Количество" class="money_inputs" name="immunity_count" value="1" id="sumFront">
 
-          <input type="submit" value="Купить" class="send">
+          <input type="submit" value="КУПИТЬ" class="send">
       </form>
 		</div>
       </div>
