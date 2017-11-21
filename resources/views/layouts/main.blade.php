80cbc2c982c0f49fbd7c418bb2a4c27e16494396
Олег Штолин <kasatka567@gmail.com>
2017-11-21 10:18:53 +0300

    console

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,6 +21,34 @@
     <link rel="stylesheet" href="/assets/lightbox/dist/css/lightbox.min.css">
     <link rel="shortcut icon" href="{{ asset('ico/faviсon.png') }}" type="image/png" />
 
+    <script type="text/javascript" >
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter46718757 = new Ya.Metrika({
+                        id:46718757,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true,
+                        webvisor:true
+                    });
+                } catch(e) { }
+            });
+
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else { f(); }
+        })(document, window, "yandex_metrika_callbacks");
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/46718757" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+
 
 	<title>Reformator.ONE</title>
 
