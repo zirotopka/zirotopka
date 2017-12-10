0f8ddc28e4cd874a5eaedbf535a232ea7685dd84
Sylar Gray <sylargray@icloud.com>
2017-12-11 01:53:27 +0500

    Fix program

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -115,7 +115,6 @@
                 <h2>ПРОГРАММЫ</h2>
                 <div class="programm_list">
                     <div class="block-program block-program-rone-lite rone_start col-lg-5 col-md-5 col-sm-5 col-xs-12">
-                        <div class="program-mask"></div>
                         <a href="/programm/r.one_lite" class="program-image" style="background: url('/image/programs/rone-lite.jpg') no-repeat;">
                             <div class="program-top" style="background: url('/image/programs/logos/logo-rone-lite.png') no-repeat;">
 
