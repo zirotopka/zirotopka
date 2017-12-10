01c77ed5f63e950841e116fac0c9ace716212e4d
Sylar Gray <sylargray@icloud.com>
2017-12-11 01:23:19 +0500

    Fix program

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -114,9 +114,9 @@
             <div class="programm-screen col-lg-12 col-md-12 col-sm-12 col-xs-12" id="section_3">
                 <h2>ПРОГРАММЫ</h2>
                 <div class="programm_list">
-                    <div class="rone_start col-lg-5 col-md-5 col-sm-5 col-xs-12">
+                    <div class="block-program block-program-rone-lite rone_start col-lg-5 col-md-5 col-sm-5 col-xs-12">
                         <a href="/programm/r.one_lite">
-                            <img src="/ico/program-start-lite.jpg" class="start" alt="">
+                            <img src="/image/programs/rone-lite.jpg" class="program-image" alt="">
                             <div class="program-description hidden-xs hidden-sm">Программа по лайт-цене для новичков</div>
                             <p class="prg1" style="display:none;">R.ONE Lite</p>
                         </a>
