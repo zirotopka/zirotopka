b3396ba32b3465bc7278299132a77be5cd9dabfe
Олег Штолин <kasatka567@gmail.com>
2017-12-13 10:44:48 +0300

    lk

--- database/seeds/ProgrammTableSeeder.php
+++ database/seeds/ProgrammTableSeeder.php
@@ -25,7 +25,7 @@ class ProgrammTableSeeder extends Seeder
             ['slug' => 'r.one_runner','name' => 'R.ONE RUNNER','description' => 'Программа предназначенная для бега.', 'days' => 28, 'tranings' => 9, 'day_off' => 9, 'tasks' => 10, 'cost' => 2500, 'status' => 0,'logo' => '/image/program/r.one_run.png','lite' => 0],
         	['slug' => 'r.one_runner_plus','name' => 'R.ONE RUNNER +','description' => 'Программа предназначенная для бега с бонусами', 'days' => 28, 'tranings' => 9, 'day_off' => 9, 'tasks' => 10, 'cost' => 2500, 'status' => 0,'logo' => '/image/program/r.one_run+.png','lite' => 0],
         	['slug' => 'r.one_power','name' => 'R.ONE POWER','description' => 'Программа предназначенная для самых сильных', 'days' => 28, 'tranings' => 9, 'day_off' => 9, 'tasks' => 10, 'cost' => 2500, 'status' => 0,'logo' => '/image/program/r.one_power.png','lite' => 0],
-            ['slug' => 'r.one_lite','name' => 'R.ONE Lite','description' => 'Программа тренировок по лайт-цене для всех, кто хочет попробовать свои силы и не любит отправлять отчеты, а также кто хочет больше внимания уделить бонусной "системе"', 'days' => 28, 'tranings' => 8, 'day_off' => 10, 'tasks' => 11, 'cost' => 1500, 'status' => 1,'logo' => '/image/program/r.one_lite.png','lite' => 1],
+            ['slug' => 'r.one_lite','name' => 'R.ONE Lite','description' => 'Программа тренировок по лайт-цене для всех, кто хочет попробовать свои силы и не любит отправлять отчеты, а также кто хочет больше внимания уделить бонусной системе', 'days' => 28, 'tranings' => 8, 'day_off' => 10, 'tasks' => 11, 'cost' => 1500, 'status' => 1,'logo' => '/image/program/r.one_lite.png','lite' => 1],
         ];
 
         foreach ( $trainings as $training ) {
